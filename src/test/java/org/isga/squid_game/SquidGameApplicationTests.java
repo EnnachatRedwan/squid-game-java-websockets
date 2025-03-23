f687248d0ae27e7feb6054a6cf71250f7fb2f110
ENNACHAT Redwan <100237071+EnnachatRedwan@users.noreply.github.com>
2025-03-23 22:30:21 +0000

    init the project and added username page

---
+++ src/test/java/org/isga/squid_game/SquidGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.isga.squid_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquidGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
